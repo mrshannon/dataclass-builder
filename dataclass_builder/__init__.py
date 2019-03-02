307453a05beab676d166ec8495b76065bd974982
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2019-03-01 18:37:16 -0700

    Add type information.

--- dataclass_builder/__init__.py
+++ dataclass_builder/__init__.py
@@ -2,9 +2,10 @@
 
 import inspect
 import itertools
-from dataclasses import _MISSING_TYPE
-from collections.abc import MutableMapping
-from cached_property import cached_property
+from dataclasses import Field, _MISSING_TYPE
+from typing import Any, Mapping, MutableMapping, Iterator
+
+from cached_property import cached_property  # type: ignore
 
 __version__ = '0.0.1a1'
 
@@ -18,7 +19,7 @@ class DataclassBuilderError(Exception):
 
 class UndefinedFieldError(DataclassBuilderError):
 
-    def __init__(self, message, dataclass, field):
+    def __init__(self, message: str, dataclass: Any, field: str) -> None:
         super().__init__(message)
         self.dataclass = dataclass
         self.field = field
@@ -26,40 +27,41 @@ class UndefinedFieldError(DataclassBuilderError):
 
 class MissingFieldError(DataclassBuilderError):
 
-    def __init__(self, message, dataclass, field):
+    def __init__(self, message: str, dataclass: Any, field: 'Field[Any]') \
+            -> None:
         super().__init__(message)
         self.dataclass = dataclass
         self.field = field
 
 
-class DataclassBuilder(MutableMapping):
+class DataclassBuilder(MutableMapping[str, Any]):
 
-    def __init__(self, dataclass, **kwargs):
+    def __init__(self, dataclass: Any, **kwargs: Any):
         # any class that has a __dataclass_fields__ attribute is considered a
         # dataclass
         assert inspect.isclass(dataclass)
         assert hasattr(dataclass, '__dataclass_fields__')
         self.__dataclass = dataclass
-        self.__dataclass_attributes = dict()
+        self.__dataclass_attributes: MutableMapping[str, Any] = dict()
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-    def __getitem__(self, item):
-        return self.__dataclass_attributes[item]
+    def __getitem__(self, key: str) -> Any:
+        return self.__dataclass_attributes[key]
 
-    def __setitem__(self, key, value):
+    def __setitem__(self, key: str, value: Any) -> None:
         self.__dataclass_attributes[key] = value
 
-    def __delitem__(self, key):
+    def __delitem__(self, key: str) -> None:
         del self.__dataclass_attributes[key]
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[str]:
         return iter(self.__dataclass_attributes)
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.__dataclass_attributes)
 
-    def __getattr__(self, item):
+    def __getattr__(self, item: str) -> Any:
         try:
             return self.__dataclass_attributes[item]
         except KeyError:
@@ -67,7 +69,7 @@ class DataclassBuilder(MutableMapping):
                 f"'{self.__class__.__name__}({self.__dataclass.__name__})' "
                 f"object has no attribute '{item}'")
 
-    def __setattr__(self, item, value):
+    def __setattr__(self, item: str, value: Any) -> None:
         if item.startswith('_' + self.__class__.__name__):
             self.__dict__[item] = value
         elif item not in self.__dataclass.__dataclass_fields__:
@@ -77,13 +79,13 @@ class DataclassBuilder(MutableMapping):
         else:
             self.__dataclass_attributes[item] = value
 
-    def __delattr__(self, item):
+    def __delattr__(self, item: str) -> None:
         if item.startswith('_' + self.__class__.__name__):
             del self.__dict__[item]
         else:
             del self.__dataclass_attributes[item]
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         args = itertools.chain(
             [self.__dataclass.__name__],
             (f'{key}={self.__dataclass_attributes[key]}'
@@ -91,7 +93,7 @@ class DataclassBuilder(MutableMapping):
              if key in self.__dataclass_attributes))
         return f"{self.__class__.__name__}({', '.join(args)})"
 
-    def __build(self):
+    def __build(self) -> Any:
         kwargs = {key: value
                   for key, value in self.__dataclass_attributes.items()
                   if key in self.__fields}
@@ -106,29 +108,31 @@ class DataclassBuilder(MutableMapping):
                         self.__dataclass, field)
 
     @cached_property
-    def __fields(self):
+    def __fields(self) -> Mapping[str, 'Field[Any]']:
         return {**self.__required_fields, **self.__optional_fields}
 
     @cached_property
-    def __required_fields(self):
+    def __required_fields(self) -> Mapping[str, 'Field[Any]']:
         return {name: field
                 for name, field in
                 self.__dataclass.__dataclass_fields__.items()
                 if _isrequired(field)}
 
     @cached_property
-    def __optional_fields(self):
+    def __optional_fields(self) -> Mapping[str, 'Field[Any]']:
         return {name: field
                 for name, field in
                 self.__dataclass.__dataclass_fields__.items()
                 if field.init and not _isrequired(field)}
 
 
-def build(builder):
+def build(builder: DataclassBuilder) -> Any:
     return getattr(builder, f'_{builder.__class__.__name__}__build')()
 
 
-def fields(builder, *, required=True, optional=True):
+def fields(builder: DataclassBuilder, *,
+           required: bool = True, optional: bool = True) \
+        -> Mapping[str, 'Field[Any]']:
     if required and optional:
         fields_ = getattr(builder, f'_{builder.__class__.__name__}__fields')
     elif required:
@@ -140,6 +144,6 @@ def fields(builder, *, required=True, optional=True):
     return {field.name: field.type for field in fields_.values()}
 
 
-def _isrequired(field):
-    return isinstance(field.default, _MISSING_TYPE) and \
-           isinstance(field.default_factory, _MISSING_TYPE) and field.init
+def _isrequired(field: 'Field[Any]') -> bool:
+    return field.init and isinstance(field.default, _MISSING_TYPE) and \
+           isinstance(field.default_factory, _MISSING_TYPE)  # type: ignore
